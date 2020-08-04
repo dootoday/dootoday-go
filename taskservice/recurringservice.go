47e2a0db2a510bf9a17b01f01cf4967765ce3810
Sudipta Sen <sanborn.sen@gmail.com>
2020-08-04 18:27:35 +0530

    recurring match function created

--- taskservice/recurringservice.go
+++ taskservice/recurringservice.go
@@ -46,16 +46,57 @@ func (ts *RecurringTaskService) IsRecurringTask(task string) (string, RecurringT
 }
 
 // DoesMatchRecurring :
-func (ts *RecurringTaskService) DoesMatchRecurring(startTime time.Time, currentTime time.Time, recurringType RecurringType) bool {
+func (ts *RecurringTaskService) DoesMatchRecurring(
+	startTime time.Time,
+	checkTime time.Time,
+	recurringType RecurringType,
+) bool {
 	// If the recurring task created before current date then
 	// current date should not match with the task
-	if currentTime.Before(startTime) {
+	if checkTime.Before(startTime) {
 		return false
 	}
 	// If the recurring type is every day then any current date
 	// should match with the task except the condition above
 	if recurringType == RecurringEveryDay {
 		return true
+	}
+	// if the recurring type is every week then any day with same
+	// week dayshould match
+	if recurringType == RecurringEveryWeek {
+		if checkTime.Weekday() == startTime.Weekday() {
+			return true
+		}
+	}
+	// if the recurring type is every month then any day with same
+	// day day should match
+	// If it's the last day of the month
+	if recurringType == RecurringEveryMonth {
+		nextDayOfStartTime := startTime.AddDate(0, 0, 1)
+		isStartLastDay := nextDayOfStartTime.Day() == 1
+		nextDayOfCheckTime := checkTime.AddDate(0, 0, 1)
+		isCheckLastDay := nextDayOfCheckTime.Day() == 1
+		if isCheckLastDay && isStartLastDay {
+			return true
+		}
+		if checkTime.Day() == startTime.Day() {
+			return true
+		}
+	}
+
+	// if the recurring type is every week then any day with same
+	// week dayshould match
+	if recurringType == RecurringEveryYear {
+		if checkTime.Month() == startTime.Month() {
+			if startTime.Month().String() == "February" &&
+				startTime.Day() == 29 {
+				nextDayOfCheckTime := checkTime.AddDate(0, 0, 1)
+				return nextDayOfCheckTime.Day() == 1
+			}
+			if checkTime.Day() == startTime.Day() {
+				return true
+			}
+		}
 	}
 	return false
 }
